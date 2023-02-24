6b65e58d96c7fc81ccaed600fe9ee6a235c7694f
tongzhou <im.tz@outlook.com>
2023-02-25 00:09:28 +0800

    Initial commit

---
+++ src/main/java/com/example/renamefile/controller/IndexController.java
@@ -0,0 +1,69 @@
+package com.example.renamefile.controller;
+
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
+import com.drew.metadata.mov.QuickTimeDirectory;
+import com.drew.metadata.mov.media.QuickTimeVideoDirectory;
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/rename")
+    public List<String> rename(@RequestParam String dirPath) {
+//        String dirPath = "E:\\宝宝照片\\202301";
+        File dir = new File(dirPath);
+        File[] files = dir.listFiles();
+        List list = new ArrayList();
+        for (File file : files) {
+            try {
+                Metadata metadata = null;
+                try {
+                    metadata = ImageMetadataReader.readMetadata(file);
+                } catch (ImageProcessingException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String name = file.getName();
+                String suffix = name.substring(name.lastIndexOf(".")).toUpperCase();
+                Date date = null;
+                switch (suffix) {
+                    case ".MOV":
+                        QuickTimeDirectory quickTimeDirectory = metadata.getFirstDirectoryOfType(QuickTimeVideoDirectory.class);
+                        date = quickTimeDirectory.getDate(QuickTimeVideoDirectory.TAG_CREATION_TIME);
+                        break;
+                    case ".JPG":
+                    case ".JPEG":
+                    case ".PNG":
+                        ExifSubIFDDirectory directory = metadata.getFirstDirectoryOfType(ExifSubIFDDirectory.class);
+                        if (directory == null) {
+                            continue;
+                        }
+                        date = directory.getDateOriginal();
+                        break;
+                }
+                if (date != null) {
+                    String format = DateFormatUtils.format(date, "yyyy-MM-dd hh_mm_ss").concat(suffix);
+                    boolean b = file.renameTo(new File(dirPath.concat("\\").concat(format)));
+                    String info = String.format("重命名：%s ==> %s %s", name, format, b);
+                    list.add(info);
+                }
+            } catch (Exception e) {
+                continue;
+            }
+
+        }
+        return list;
+    }
+
+}
---
+++ src/test/java/com/example/renamefile/RenameFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.renamefile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenameFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
